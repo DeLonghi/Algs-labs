6924be72c3ac95eb2f2b1f295861ebfb28afc36c
Ilya <vinnikov_i@mail.ru>
2021-11-30 01:08:40 +0300

    Revert "Lab 8"
    This reverts commit e67d5f5be2c0256480ed621fbaa8255e93333461.

--- lab8/alg1.py
+++ lab8/alg1.py
@@ -11,9 +11,9 @@ LETTERS = string.ascii_letters
 def lcs(X, Y):
     m = len(X)
     n = len(Y)
-  
+
     L = [[None]*(n + 1) for i in range(m + 1)]
-  
+
     for i in range(m + 1):
         for j in range(n + 1):
             if i == 0 or j == 0 :
@@ -45,7 +45,7 @@ def measure_time(n_runs, s1, s2, linear=False):
                     start_time = timer()
                     lcs(_s1, _s2)
                     times[i].append(timer() - start_time)
-            
+
     return np.array(times, dtype=np.float64).mean(axis=0)
 
 times = measure_time(5, ''.join(random.choice(LETTERS) for i in range(NUMBER_OF_EXPEREMENTS)),''.join(random.choice(LETTERS) for i in range(NUMBER_OF_EXPEREMENTS)), True)
