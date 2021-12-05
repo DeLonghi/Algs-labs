f457fd40056ddd17ed8465c42ada57a37da3bdc6
Ilya <vinnikov_i@mail.ru>
2021-12-05 20:30:04 +0300

    Fixed graphs

--- lab3/task2.py
+++ lab3/task2.py
@@ -4,27 +4,23 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 from sklearn.linear_model import SGDRegressor
 
-Eps = 0.001 
+Eps = 0.0001 
 K = 101
-a = random.uniform(0, 1)
-b = random.uniform(0, 1)
-print(a, b)
+alpha = np.random.rand(1)
+beta = np.random.rand(1)
 
 mu, sigma = 0, 0.1
 s = np.random.normal(0, 1, 101)
 
 
 def fL(k, x):
-    return a * x + b + s[k]
-
-def fR(k, x):
-    return (b + s[k]) / (1 + a * x )
+    return alpha * x + beta + s[k]
 
 def linear(x, a, b):
     return a * x + b
 
 def rational(x, a, b):
-    return b / (1 + a * x)
+    return a / (1 + b * x)
 
 def funcToOptLin(a_b, *params):
     x, y = params
@@ -42,13 +38,10 @@ def funcToOptRat(a_b, *params):
     sum = 0
     for i in range(K):
         sum += (rational(x[i], a, b) - y[i])**2
+    # print(sum)
     return sum
 
-def jacobian(a_b, *params):
-    # x, y = params
-    a = a_b[0]
-    b = a_b[1]
-    return np.array((-2*.5*(1 - a) - 4*a*(b - a**2), 2*(b - a**2)))
+
 
 def form_data(f):
     y = []
@@ -118,16 +111,16 @@ def ResToOptRat(a_b, *params):
 params = (x_values, y)
 
 
-res_gd = optimize.minimize(funcToOptLin, (0.3, 0.5), method='BFGS', args=params, tol=Eps, options={'disp':True}, jac=getGradLin)
-res_cg = optimize.minimize(funcToOptLin, (0.3, 0.5), method='CG', args=params, tol=Eps, options={'disp':True})
-res_N_cg = optimize.minimize(funcToOptLin, (0.3, 0.5), method='Newton-CG', args=params, tol=Eps,  options={'disp':True, 'xtol': Eps}, jac=getGradLin)
-res_lma = optimize.least_squares(ResToOptLin, (0.3, 0.5), method='lm', args=params, xtol=Eps)
+res_gd = optimize.minimize(funcToOptLin, [0, 0], method='BFGS', args=params, tol=Eps, options={'disp':True})
+res_cg = optimize.minimize(funcToOptLin, [0, 0], method='CG', args=params, tol=Eps, options={'disp':True})
+res_N_cg = optimize.minimize(funcToOptLin, [0, 0], method='Newton-CG', args=params, tol=Eps,  options={'disp':True}, jac=getGradLin)
+res_lma = optimize.least_squares(ResToOptLin, [0, 0], method='lm', args=params, xtol=Eps)
 x_values = np.array(x_values)
 
 
 fig, axes = plt.subplots()
 plt.plot(x_values, y, label='Generated data')
-plt.plot(x_values, x_values * a + b, label='Initial line')
+plt.plot(x_values, x_values * alpha + beta, label='Initial line')
 plt.plot(x_values, x_values * res_gd.x[0] + res_gd.x[1], label='Gradient descent method')
 plt.plot(x_values, x_values * res_cg.x[0] + res_cg.x[1], label='Conjugate gradient method')
 plt.plot(x_values, x_values * res_N_cg.x[0] + res_N_cg.x[1], label='Newton method')
@@ -138,19 +131,20 @@ axes.set_ylabel('Y')
 plt.legend()
 plt.grid()
 
-res_gd = optimize.minimize(funcToOptRat, (0.7, 0.3), method='BFGS', args=params, tol=Eps, options={'disp':True}, jac=getJacobianRat)
-res_cg = optimize.minimize(funcToOptRat, (0.2, 0.5), method='CG', args=params,  options={'disp':True})
-res_N_cg = optimize.minimize(funcToOptRat, (0.2, 0.2), method='Newton-CG', args=params, tol=Eps, options={'disp':True}, jac=getJacobianRat)
-res_lma = optimize.least_squares(ResToOptRat, (0.2, 0.2), method='lm', args=params)
+res_gd = optimize.minimize(funcToOptRat, [0, 0], method='BFGS', args=params, tol=Eps, options={'disp':True})
+res_cg = optimize.minimize(funcToOptRat, [0, 0], method='CG', args=params, tol=Eps, options={'disp':True})
+res_N_cg = optimize.minimize(funcToOptRat, [0, 0], method='Newton-CG', args=params, tol=Eps, options={'disp':True}, jac=getJacobianRat)
+res_lma = optimize.least_squares(ResToOptRat, [0, 0], method='lm', args=params)
+# x_values = np.array(x_values)
 
 
 fig, axes = plt.subplots()
 plt.plot(x_values, y, label='Generated data')
-plt.plot(x_values, x_values * a + b, label='Initial line')
-plt.plot(x_values, x_values * res_gd.x[0] + res_gd.x[1], label='Gradient descent method')
-plt.plot(x_values, x_values * res_cg.x[0] + res_cg.x[1], label='Conjugate gradient method')
-plt.plot(x_values, x_values * res_N_cg.x[0] + res_N_cg.x[1], label='Newton method')
-plt.plot(x_values, x_values * res_lma.x[0] + res_lma.x[1], label='Levenberg-Marquardt method')
+plt.plot(x_values, x_values * alpha + beta, label='Initial line')
+plt.plot(x_values, res_gd.x[0] / (1 + x_values * res_gd.x[1]), label='Gradient descent method')
+plt.plot(x_values, res_gd.x[0] / (1 + x_values * res_gd.x[1]), label='Conjugate gradient method')
+plt.plot(x_values, res_gd.x[0] / (1 + x_values * res_gd.x[1]), label='Newton method')
+plt.plot(x_values, res_gd.x[0] / (1 + x_values * res_gd.x[1]), label='Levenberg-Marquardt method')
 axes.set_title("Rational approximation")
 axes.set_xlabel('X')
 axes.set_ylabel('Y')
